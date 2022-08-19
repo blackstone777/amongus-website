030ed7fce6917659e296b57192d045196dcdd624
Json <josuepach77@gmail.com>
2022-08-19 10:23:31 -0400

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", (req, res) => res.render("index", { title: 'first-page' }));
+
+
+router.get("/about", (req, res) => res.render("about", { title: 'about-me' }));
+
+
+router.get("/contact", (req, res) => res.render("contact", { title: 'contact-me' }));
+
+
+export default router;
